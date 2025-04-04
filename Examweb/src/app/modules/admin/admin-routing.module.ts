aa6a4197c4aee2f6c2a265a9b0fd5b088f4e6c25
nityahs <148626044+nityahs@users.noreply.github.com>
2025-04-05 02:42:53 +0530

    add question redirect

--- Examweb/src/app/modules/admin/admin-routing.module.ts
+++ Examweb/src/app/modules/admin/admin-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CreateTestComponent } from './components/create-test/create-test.component';
+import { AddQuestionInTestComponent } from './components/add-question-in-test/add-question-in-test.component';
 
 const routes: Routes = [
-{path:'dashboard', component:DashboardComponent},
-{path:'create-test', component:CreateTestComponent},
-
-
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'create-test', component: CreateTestComponent },
+  { path: 'add-question/:id', component: AddQuestionInTestComponent }
 ];
 
 @NgModule({
