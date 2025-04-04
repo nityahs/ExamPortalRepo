f41d77f98ac15bd95f2137f9988c0443f2003d8d
nityahs <148626044+nityahs@users.noreply.github.com>
2025-04-05 03:00:31 +0530

    Question Option

--- Examweb/src/app/modules/admin/components/add-question-in-test/add-question-in-test.component.ts
+++ Examweb/src/app/modules/admin/components/add-question-in-test/add-question-in-test.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-add-question-in-test',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './add-question-in-test.component.html',
   styleUrl: './add-question-in-test.component.scss'
 })
 export class AddQuestionInTestComponent {
 
+  constructor(
+    private fb: FormBuilder,
+    private adminService: AdminService,
+    private notification: NzNotificationService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+  id: number | null;
+  questionForm!: FormGroup;
+
+  ngOnInit() {
+    this.questionForm = this.fb.group({
+      questionText: [null, [Validators.required]],
+      optionA: [null, [Validators.required]],
+      optionB: [null, [Validators.required]],
+      optionC: [null, [Validators.required]],
+      optionD: [null, [Validators.required]],
+      correctOption: [null, [Validators.required]],
+    });
+  }
+  
 }
+
