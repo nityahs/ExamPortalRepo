b66b5babd840c08ac7190dba23db2ae6fec2385a
nityahs <148626044+nityahs@users.noreply.github.com>
2025-04-05 02:28:10 +0530

    adding pages

---
+++ Examweb/src/app/modules/admin/components/add-question-in-test/add-question-in-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-question-in-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-question-in-test.component.html',
+  styleUrl: './add-question-in-test.component.scss'
+})
+export class AddQuestionInTestComponent {
+
+}
